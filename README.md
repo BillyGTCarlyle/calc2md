896945ae14c696e06b1de5fd37a851ea12c81ff8
Billy Carlyle <billy.carlyle@durham.ac.uk>
2026-05-22 16:07:06 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+### calc2md
+
+In a similar fashion to calc2LaTeX, this macro makes it simple to turn libreoffice data into plaintext for use in markdown documents.  
+Simply select the area you would like to turn into a table, then run the macro.
---
+++ scripts/markdown.py
@@ -0,0 +1,39 @@
+import uno
+import msgbox
+
+document = XSCRIPTCONTEXT.getDocument()
+header = True
+
+def toMarkdown():
+    smgr = XSCRIPTCONTEXT.getComponentContext().getServiceManager()
+    dp = smgr.createInstanceWithContext("com.sun.star.awt.DialogProvider", XSCRIPTCONTEXT.getComponentContext())
+    controller = document.CurrentController
+    active_sheet = controller.ActiveSheet
+    selection = controller.getSelection()
+    data = selection.getDataArray()
+    dlg = dp.createDialog("vnd.sun.star.script:calc2md.markdown?location=application")
+    text = dlg.getControl("MarkdownOutput")
+    dataString = ""
+    if header:
+        dataString += "| "
+        for column in data[0]:
+            dataString += str(column)
+            dataString += " | "
+        dataString += '\n'
+        for i in range(0,len(data[0])):
+            dataString += "| --- "
+            
+        dataString += "|\n"
+        data = data[1:]
+
+    for row in data:
+        dataString += "| "
+        for column in row:
+            dataString += str(column)
+            dataString += " | "
+
+        dataString += '\n'
+    text.Model.Text = dataString
+    dlg.Model.Title = "Markdown Table"
+    dlg.execute()
+    dlg.dispose()
